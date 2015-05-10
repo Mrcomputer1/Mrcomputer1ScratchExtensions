17a8db2b9cf3dd7fb918a3f5625200286cb92682
Mrcomputer1 <Mrcomputer1@users.noreply.github.com>
2015-05-10 17:29:17 +1000

    Update MinecraftExtension.js

--- MinecraftExtension.js
+++ MinecraftExtension.js
@@ -5,7 +5,7 @@
 		return {status:2, msg: 'Ready'};
 	};
 	
-	ext.isOnline = function(serverIP, serverPORT) {
+	ext.isOnline = function(serverIP, serverPORT, callback) {
 		$.ajax({
 			url: 'http://mcapi.us/server/status?ip='+serverIP+"&port="+serverPORT,
 			dataType: 'jsonp',
