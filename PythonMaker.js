5e66e7d3eff060b7d9e5ca7dd03b7667f1ee7edc
Mrcomputer1 <Mrcomputer1@users.noreply.github.com>
2015-05-10 17:01:36 +1000

    Update PythonMaker.js

--- PythonMaker.js
+++ PythonMaker.js
@@ -1,30 +1,40 @@
 (function(ext) {
-	
-	var save = "";
-	
-	ext._shutdown = function() {};
-	
-	ext._getStatus = function() {
-		return {status:2,msg:"Ready"};
-	};
-	
-	ext.cmd_print = function() {
-		save = save + "/n/n/n";
-	};
-	
-	ext.build = function() {
-		var gui = window.open("", "Code", "location=no,menubar=no,resizable=no,status=no,toolbar=no");
-		save = save.replace("/n/n/n", "\n");
-		gui.document.write("<textarea>"+save+"</textarea>");
-		save = "";
-	};
-	
-	var blocks{
-		blocks: [
-			[' ', 'BUILD', 'build'],
-			[' ', 'print %s', 'cmd_print', 'Hello!'],
-		]
-	};
-	
-	ScratchExtensions.register("Python Maker", blocks, ext);
+    var alarm_went_off = false; // This becomes true after the alarm goes off
+
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.set_alarm = function(time) {
+       window.setTimeout(function() {
+           alarm_went_off = true;
+       }, time*1000);
+    };
+
+    ext.when_alarm = function() {
+       // Reset alarm_went_off if it is true, and return true
+       // otherwise, return false.
+       if (alarm_went_off === true) {
+           alarm_went_off = false;
+           return true;
+       }
+
+       return false;
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['', 'run alarm after %n seconds', 'set_alarm', '2'],
+            ['h', 'when alarm goes off', 'when_alarm'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Alarm extension', descriptor, ext);
 })({});
